bef3464ca1a89562a40a546969a658e1bbed5a97
鲁正佼 <3038413299@qq.com>
2022-12-01 15:39:18 +0800

    feat(新功能):初始化、引入全局、动画库、字体图标库以及设置tabbar底部导航栏

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 全局样式文件 */
+	@import url("@/common/common.css");
+	@import url("@/common/free.css");
+	/* 引入动画库 */
+	@import url("@/common/animate.min.css");
+	/* 引入icon */
+	@import url("~@/common/iconfont.css");
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view> -->
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
